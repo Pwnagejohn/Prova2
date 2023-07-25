803f69c0afb863a22e42cb9f977df1440f635ca2
Pwnagejohn <Joe_Black89@hotmail.it>
2023-07-25 22:28:47 +0200

    AddedFiles
    adds

---
+++ src/main/kotlin/com/kb/Prova2/Prova2Application.kt
@@ -0,0 +1,11 @@
+package com.kb.Prova2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Prova2Application
+
+fun main(args: Array<String>) {
+	runApplication<Prova2Application>(*args)
+}
